bc1a2ec0ab8452b92a916809f5f63a3c62855b59
Elliott Ventura <elliott.ventura26@gmail.com>
2023-04-03 09:26:45 -0500

    worked on home page

--- tickit/src/components/Home.jsx
+++ tickit/src/components/Home.jsx
@@ -1,46 +1,27 @@
+import {useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Nav from './Nav'
 
-import Nav from "./Nav"
-import { Link } from "react-router-dom"
-import { useState } from "react"
 
 export default function Home(){
 
+    let navigate = useNavigate();
+    const [events, setEvents] = useState([])
+    useEffect(()=>{
+        const getAllEvents = async () => {
+            const res = await axios.get('http://localhost:8000/events/?format=json');
+            setEvents(res.data)
+        };
+        getAllEvents();
+    },[]);
+    
+    const showDetail = (index)=>{
+        navigate(`events/${index}`)
+      }    
 
-const[event, setEvent]= useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
 
-const template=(index)=>{
 
-    let eventV = (
-
-        <div className="events-box"> 
-            <div className="image">
-                <img className="events-img" src="https://media.gettyimages.com/id/978980792/photo/alex-turner-of-arctic-monkeys-performs-at-coca-cola-roxy-on-june-19-2018-in-atlanta-georgia.jpg?s=612x612&w=0&k=20&c=XepG2gHNuLqlzeezkocFiYC7x_DR1TWKoGnpG0dojLw=" />
-        
-            </div>
-            <div className="event-date">
-                <div className="img-details">
-                <h4>SUN | AUG 11 7:00PM</h4>
-                <span className="img-loco">Forest Hills, NY Forest Hills Stadium</span>
-
-                <span className="img-title">Arctic Monkeys</span>
-                </div>
-            </div>
-        </div> 
-
-    )
-    return (eventV)
-}
-
-    function multi(){
-        return(
-            event.map((event,index)=>(
-             <div key={index}>
-                {template(index)}
-             </div>
-              
-            ))
-        )
-    }
     return(
 
         <div className="home">
@@ -66,15 +47,36 @@ const template=(index)=>{
                             </ul>
                         </span>
                     </btn>
-                    {/* <img className = "hero-img"src="https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?cs=srgb&dl=pexels-sebastian-ervi-1763075.jpg&fm=jpg" width="850px" height="600px" ></img> */}
                 </div>
                 <div className="upcoming-events-container">
                     <div className="upcoming-event-inner">
-                        {/* <h2 className="upcoming-event-text">Checkout Upcoming Events</h2>
-                        <btn className="upcoming-events-btn">See All</btn> */}
+
                     </div>
                     <div className="upcoming-events-list">
-                    {/* {multi()} */}
+
+
+                    <div className= 'card-container'>
+                {events.map((event)=>(
+                    <div onClick={() => showDetail(event.id)} key={event.id}className="mapped-card-display">
+                        <h3>{event.name}</h3>
+                        <div className="img-wrapper">
+                            <img src = {event.image_url}/>
+                        </div>
+                        <h5>Date: {event.date}</h5>
+                        <h5>Time: {event.start_time}</h5>
+                    </div>
+                ))}
+            </div>
+
+
+
+
+
+
+
+
+
+
                     </div>
              
                 </div> 
