eba1e014667bde48470c8fd42c22dbcc48898e18
Yael Kaufman <ylkauf@gmail.com>
2023-04-04 08:18:33 -0700

    updated confirmation page

--- tickit/src/components/Confirmation.jsx
+++ tickit/src/components/Confirmation.jsx
@@ -1,9 +1,11 @@
 import Nav from "./Nav"
+import {Link} from 'react-router-dom'
 export default function Confirmation(){
     return(
         <div>
             <Nav/>
             <h4>Congrats! Your purchase is confirmed. Enjoy the show! </h4>
+            <Link to="/events"><button>Browse More Events</button></Link>
         </div>
     )
 }
